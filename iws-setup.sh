b3db0ab82e7b797aaec978cc43d540b463c7a55a
marvinwulf <m.wulf208@gmail.com>
2024-03-19 21:11:00 +0100

    Added shell util

---
+++ iws-setup.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+echo -e "\033[0;32mStarting IWS Server setup..\033[0m"
+
+# Change directory to /root
+cd /root/
+echo
+
+# Prompt user for static IP, gateway, and subnet mask
+echo -e "\033[0;33mEnter desired port for MQTT (default: 1883):\033[0m"
+read port
+
+echo
+# Update the package list
+sudo apt update -y
+
+echo -e "\033[0;32mInstalling mosquitto broker.\033[0m"
+
+# Install and configure Mosquitto
+sudo apt install mosquitto -y
+systemctl start mosquitto
+systemctl enable mosquitto
+echo "$(systemctl status mosquitto)"
+
+config_file="/etc/mosquitto/mosquitto.conf"
+
+# Add the line 'allow_anonymous true' if it doesn't exist, otherwise update it
+if ! grep -q "allow_anonymous true" "$config_file"; then
+    echo "allow_anonymous true" >> "$config_file"
+else
+    sed -i "s/^allow_anonymous .*/allow_anonymous true/" "$config_file"
+    echo "allow_anonymous true" >> "$config_file"
+fi
+
+# Add or update the listener port
+if ! grep -q "^listener $port" "$config_file"; then
+    echo "listener $port" >> "$config_file"
+else
+    sed -i "s/^listener .*/listener $port/" "$config_file"
+    echo "listener $port" >> "$config_file"
+fi
+
+echo -e "\033[0;32mInstalling Python and virtual environment.\033[0m"
+
+# Install Python dependencies
+sudo apt install python3.11-venv python3-pip -y
+python3 -m venv /root/.iws
+source /root/.iws/bin/activate
+pip3 install paho-mqtt datetime requests schedule
+deactivate
+
+echo -e "\033[0;32mInstalling the IWS Server scripts.\033[0m"
+
+# Install additional tools
+sudo apt install tmux wget unzip sqlite3 -y
+
+# Download and set up IWS
+cd /root/.iws/
+wget https://github.com/marvinwulf/IWS/archive/main.zip
+unzip main.zip
+rm main.zip
+
+cd /root/.iws/IWS-main/
+sed -i "s/^port=.*/port=$port/" "config.txt"
+echo "Port updated to $port in config.txt"
+
+echo -e "\033[0;32mInstalling the Node.js Server.\033[0m"
+
+# Install Node.js and npm, set up local server for IWS
+cd /root/.iws/IWS-main/localserver
+sudo apt install nodejs npm -y
+npm init -y
+npm install express sqlite3
+
+echo -e "\033[0;36mIWS Server Installation finished.\033[0m"
---
+++ iws-start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+# commands
+cd /root/
+
+# Create a new tmux session named "iws-main-session"
+tmux new-session -d -s iws-main-session
+
+# Split the window into four panes
+tmux split-window -t iws-main-session:0 -h
+tmux split-window -t iws-main-session:0 -v
+tmux split-window -t iws-main-session:0 -h
+
+# Select the layout for equal vertical and horizontal panes
+tmux select-layout -t iws-main-session:0 tiled
+
+# Send commands to each pane
+tmux send-keys -t iws-main-session:0.0 "systemctl start mosquitto" C-m
+tmux send-keys -t iws-main-session:0.0 "systemctl status mosquitto" C-m
+tmux send-keys -t iws-main-session:0.0 "source /root/.iws/bin/activate && cd /root/.iws/IWS-main/ && python3 handle.py" C-m
+
+tmux send-keys -t iws-main-session:0.1 "source /root/.iws/bin/activate && cd /root/.iws/IWS-main/ && python3 main.py" C-m
+
+tmux send-keys -t iws-main-session:0.2 "cd /root/.iws/IWS-main/localserver" C-m
+tmux send-keys -t iws-main-session:0.2 "node server.js" C-m
+
+# Attach to the session to view the window
+tmux attach-session -t iws-main-session
